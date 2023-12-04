d2e9421695e73921749cd52ef2bf8fd3d471f0a5
yubin <kimu2371@naver.com>
2023-12-04 19:27:52 +0900

    feat: Create urn request feature

--- posting.py
+++ posting.py
@@ -1,8 +1,9 @@
 import requests
 import os
 import dotenv
+from pydantic import BaseModel
 
-class Posting():
+class Posting(BaseModel):
   url:str
   title:str
   content:str
@@ -10,8 +11,20 @@ class Posting():
 dotenv.load_dotenv()
 ACCESS_TOKEN=os.getenv("access_toekn")
 
+def get_user_urn():
+  url='https://api.linkedin.com/v2/userinfo'
+  headers={'Authorization': f'Bearer {ACCESS_TOKEN}'}
+  response=requests.get(url,headers=headers)
+
+  if response.status_code ==200:
+    user_data=response.json()
+    user_urn=user_data['sub']
+    return user_urn
+  else:
+    print(f'error : {response.status_code} {response.text}')
+
 def post_to_linkedin(posting:Posting):
-  CLIENT_URN=os.getenv("client_secrete")
+  CLIENT_URN=get_user_urn()
   url ="https://api.linkedin.com/v2/ugcPosts"
   headers = {
         'LinkedIn-Version': '202210',
@@ -53,4 +66,7 @@ def post_to_linkedin(posting:Posting):
   else :
     print(f"Fail linkedin posting : {response.status_code}, {response.text}")
 
+
+if __name__ == "__main__":
+  post_to_linkedin(Posting(url="https://gogumac.github.io/", title="testdsdfds", content="testing auto post!"))
 
