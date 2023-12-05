0518dfea9ff433ba7008ec06c6c3135fc6602158
yubin <kimu2371@naver.com>
2023-12-05 19:21:03 +0900

    chore: Remove redundancy

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-import configparser as parser
-import requests
-from requests.auth import HTTPBasicAuth
 import auth
+from posting import Posting
+import posting
 
 app=FastAPI()
+
 
 @app.get("/api")
 async def root():
@@ -15,16 +15,12 @@ async def renew_access_token(code:str):
     res= await auth.get_access_token(code)
     return res
 
+@app.post("/api/post")
+async def request_posting(posting:Posting):
+    posting.post_to_linkedin(posting)
+
 
 if __name__ == "__main__":
-    properties=parser.ConfigParser()
-    properties.read('./config.ini')
-    url_config=properties['URL']
-    client_config=properties['CLIENT']
-    CLIENT_ID=client_config['client_id']
-    CLIENT_SECRETE=client_config['client_secrete']
-    REDIRECT_URL=url_config['redirect_url']
-    
     import uvicorn
     uvicorn.run(app,host="0.0.0.0",port=8000)
 
