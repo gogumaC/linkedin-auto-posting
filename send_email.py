f88abfac5569b212003a741408fa960cc1542c0a
yubin <kimu2371@naver.com>
2023-12-05 19:19:37 +0900

    feat: Add post report email send

--- send_email.py
+++ send_email.py
@@ -11,16 +11,18 @@ load_dotenv()
 account=os.getenv('account')
 pw=os.getenv('password')
 
-def send_auth_email(email,auth_url):
+smtp=smtplib.SMTP_SSL('smtp.gmail.com',465)
+smtp.login(account,pw)
+
+email=os.getenv("client_email")
+
+def send_auth_email(auth_url):
     reg = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
     if re.match(reg,email):
 
-        smtp=smtplib.SMTP_SSL('smtp.gmail.com',465)
-        smtp.login(account,pw)
-
         msg=MIMEMultipart()
-        msg["Subject"]="yubin-server linkedin Auth request!"
+        msg["Subject"]="linkedin Auth request!"
         msg["From"]=account
         msg["To"]=email
 
@@ -31,5 +33,45 @@ def send_auth_email(email,auth_url):
 
         smtp.quit()
         print(f"email sended!\n\n")
+    else :
+        print("email format not valid")
+
+def send_posing_complete_email(posting_title,link):
+    reg = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+    if re.match(reg,email):
+
+        msg=MIMEMultipart()
+        msg["Subject"]=f"Upload Posting to LinkedIn "
+        msg["From"]=account
+        msg["To"]=email
+
+        content=f"Uploading Posting to LinkedIn Successfully : {posting_title} \n{link}"
+        content_part=MIMEText(content,"plain")
+        msg.attach(content_part)
+        smtp.sendmail(account,email,msg.as_string())
+
+        smtp.quit()
+        print(f"posting complete email sended!\n\n")
+    else :
+        print("email format not valid")
+
+def send_posing_fail_email(posting_title,code,text):
+    reg = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+    if re.match(reg,email):
+
+        msg=MIMEMultipart()
+        msg["Subject"]=f"Fail Posting to LinkedIn"
+        msg["From"]=account
+        msg["To"]=email
+
+        content=f"Fail Uploading Posting to LinkedIn : {posting_title} \n\ncode: {code} \ntext: {text}"
+        content_part=MIMEText(content,"plain")
+        msg.attach(content_part)
+        smtp.sendmail(account,email,msg.as_string())
+
+        smtp.quit()
+        print(f"posting fail email sended!\n\n")
     else :
         print("email format not valid")
