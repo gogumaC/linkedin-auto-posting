2eaf61e0ffde13b4ca5943036ee8fb6e9f6d38c9
yubin <kimu2371@naver.com>
2023-12-06 12:27:27 +0900

    chore: Organize APIs

--- server.py
+++ server.py
@@ -1,24 +1,12 @@
 from fastapi import FastAPI
 import auth
-from posting import Posting
-import posting
 
 app=FastAPI()
-
-
-@app.get("/api")
-async def root():
-    return {"message":"yubin server"}
 
 @app.get("/linkedin")
 async def renew_access_token(code:str):
     res= await auth.get_access_token(code)
     return res
-
-@app.post("/api/post")
-async def request_posting(posting:Posting):
-    posting.post_to_linkedin(posting)
-
 
 if __name__ == "__main__":
     import uvicorn
