fad852e3bfbf1506524bcda38b8b2fd431410bb1
gogumaC(mac mini) <kimu2371@naver.com>
2024-05-19 13:28:40 +0900

    chore: move shell script directory

--- feed_parse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /home/yubin/linkedin-auto-posting/.venv/bin/activate
-python3 /home/yubin/linkedin-auto-posting/main.py
---
+++ source/feed_parse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# source /home/yubin/linkedin-auto-posting/.venv/bin/activate
+#python3 /home/yubin/linkedin-auto-posting/main.py#
+
+python3 main.py 
+python3 server.py 
