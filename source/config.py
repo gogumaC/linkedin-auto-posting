41227c3fe16410ac3f28b608aa79db1c80c52283
yubin <kimu2371@naver.com>
2024-07-14 17:10:57 +0900

    feat: Handle when .env file not exist

--- source/config.py
+++ source/config.py
@@ -5,17 +5,22 @@ from dotenv import load_dotenv
 class Config:
     _instance=None
     dotenv_path=""
+    has_file=False
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance=super(Config,cls).__new__(cls)
             dir_name=os.path.dirname(os.path.abspath(__file__))
             cls.dotenv_path=os.path.join(dir_name,".env")
+            
             if not os.path.exists(cls.dotenv_path):
                 dir_name=os.path.dirname(dir_name)
                 cls.dotenv_path=os.path.join(dir_name,".env")
             print(f"env path : {cls.dotenv_path}\n")
-            load_dotenv(cls.dotenv_path)
+            
+            if os.path.exists(cls.dotenv_path):
+                load_dotenv(cls.dotenv_path)
+                cls.has_file=True
         return cls._instance
     
     @staticmethod
